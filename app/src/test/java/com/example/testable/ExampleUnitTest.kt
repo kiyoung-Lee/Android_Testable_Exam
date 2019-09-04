0fb8e3cb429113ed146d32db9db7a47fe7785778
KiyoungLee <eevee300@gmail.com>
2019-09-05 00:16:38 +0900

    mockito 의존성 추가

--- app/src/test/java/com/example/testable/ExampleUnitTest.kt
+++ app/src/test/java/com/example/testable/ExampleUnitTest.kt
@@ -3,12 +3,15 @@ package com.example.testable
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.internal.runners.JUnit38ClassRunner
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
